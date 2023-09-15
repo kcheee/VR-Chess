49675ec58355de61e9cc98023158d844e6f1f645
JCURVEs <jaehee2173@gmail.com>
2023-09-15 18:40:52 +0900

    폰무브 주석 날라건거 해결

--- Assets/CJH/01.Scripts/J_PawnMove.cs
+++ Assets/CJH/01.Scripts/J_PawnMove.cs
@@ -1,32 +1,26 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class J_PawnMove : MonoBehaviour
 {
-
     [SerializeField]
     public float moveSpeed = 5f;
-    public bool isMoving = false; // �����̴���
+    public bool isMoving = false; // 움직이는지
 
     Animator anim;
     public int currentX;
     public int currentY;
     private Vector3 targetPosition;
-    Chessman[,] ch = new Chessman[8,8];
+    Chessman[,] ch = new Chessman[8, 8];
 
-    float myAngle; //���� �������� ����
+    float myAngle; //내가 움직였던 각도
     private void Start()
     {
-        myAngle = transform.eulerAngles.y; 
+        myAngle = transform.eulerAngles.y;
         anim = GetComponentInChildren<Animator>();
     }
-   
+
     private void Update()
     {
         currentX = (int)transform.position.x;
@@ -35,14 +29,15 @@ public class J_PawnMove : MonoBehaviour
         {
             PawnMove(1, 2);
         }
-        if(Input.GetKeyDown(KeyCode.B)) {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
             //StartCoroutine(RotatePiece(45, 1));
             PawnMove(0, 3);
         }
         if (Input.GetKeyDown(KeyCode.N))
         {
             PawnMove(5, 5);
-                     
+
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
@@ -51,17 +46,20 @@ public class J_PawnMove : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.T))
         {
-            
+
             //PawnMove(0, 0);
             Attack(1, 1);
         }
     }
+
+    //모든 말들의 움직임을 계산하는 함수 (현재 PawnMove랑 같음)
     void PieceMove(int targetX, int targetY)
     {
         Vector3 targetPos = new Vector3(targetX, 0, targetY) - transform.position;
         float dot = Vector3.Dot(transform.right, targetPos);
+        //내적을 통해 회전할지를 정한다.
         int dir = (dot > 0) ? 1 : (dot < 0) ? -1 : (Vector3.Dot(transform.forward, targetPos) < 0) ? 1 : 0;
-
+        //상대방과 나와의 각도를 잰다
         float angle = Vector3.Angle(transform.forward, targetPos);
         StartCoroutine(RotatePiece(angle * dir, (1.0f / 45) * angle, targetX, targetY, true));
 
@@ -70,131 +68,136 @@ public class J_PawnMove : MonoBehaviour
 
     float angle = 0;
     int nDir = 0;
-    //��� ������ �������� ����ϴ� �Լ� 
+    //모든 말들의 움직임을 계산하는 함수 
     public void PawnMove(int targetX, int targetY)
     {
-        Debug.Log("����");
-        nDir = 0;
-        Vector3 targetPos =  new Vector3 (targetX, 0, targetY) - transform.position; 
+        Debug.Log("실행");
+        //nDir = 0;
+        Vector3 targetPos = new Vector3(targetX, 0, targetY) - transform.position;
         float dot = Vector3.Dot(transform.right, targetPos);
-
+        nDir = (dot > 0) ? 1 : (dot < 0) ? -1 : (Vector3.Dot(transform.forward, targetPos) < 0) ? 1 : 0;
+        #region 1if
         if (dot > 0)
         {
             nDir = 1;
-            print("�����ʿ� ȸ���ؾ���");
+            print("오른쪽에 회전해야해");
         }
         else if (dot < 0)
         {
             nDir = -1;
-            print("���ʿ� ȸ���ؾ���");
+            print("왼쪽에 회전해야해");
         }
-        else // dot = 0 ��/��
+        else // dot = 0 앞/뒤
         {
             float d = Vector3.Dot(transform.forward, targetPos);
-            if (d < 0) //��
+            if (d < 0) //뒤
             {
                 nDir = 1;
-                print("�ڷ� ���ƶ�");
+                print("뒤로 돌아라");
             }
-            else //��
+            else //앞
             {
                 nDir = 0;
-                print("ȸ�� ���� ���ƶ�");
+                print("회전 하지 말아라");
             }
         }
+        #endregion
+        //상대방과 나와의 각도를 잰다
         angle = Vector3.Angle(transform.forward, targetPos);
         //StartCoroutine(Attack(targetX, targetY));
         StartCoroutine(RotatePiece(angle * nDir, (1.0f / 45) * angle, targetX, targetY, true));
-        
-        return;
 
+        return;
+        #region 전코드
+
+        #endregion
     }
+    //1. 위치값을 받는다
+    //2. 적이 있는지 확인한다
+    //3. 있으면 원래 위치까지 움직이는 것이 아니라 -1만큼 가서
+    //4. 때리는 애니메이션 하고 다시+1만큼 간다
+    //++다시 앞으로 회전한다.
     public void Attack(int targetX, int targetY)
     {
+        //적이 있는지 확인한다. => 내가 가고자하는 목표값에 적이 있다면
+        //적인지 구분하는 것 : Chessman 코드의 bool값의 isWhite 
 
+
+        //ch.SetValue(targetX, targetY);
+        //ch[targetX, targetY];
+        //
         Chessman targetPosition = ch[targetX, targetY];
-
-        if(ch[targetX, targetY].isWhite == false)
+        // 적이라면
+        if (ch[targetX, targetY].isWhite == false)
         {
-            Debug.Log("������ ����� ��");
+            Debug.Log("무조건 띄워야 함");
 
-        }   
+        }
 
     }
-
-
+    //직선이동(완료)
     IEnumerator StraightMove(int targetX, int targetY)
     {
-        
+        //초기 위치
         Vector3 currentPos = transform.position;
-       
+        //타겟의 위치
         targetPosition = new Vector3(targetX, 0, targetY);
-        
-        
+
+        //흐른시간 체크
         float elapsedTime = 0;
-      
+        //거리잰다
         float dist = Vector3.Distance(transform.position, targetPosition);
-      
+        //시간
         float duration = dist / moveSpeed;
 
 
-    
+        //타겟의 방향
         Vector3 dir = transform.forward;
         anim.Play("Move", 0, 0);
-        while (elapsedTime / duration <  1 /* ���� ������ */)
+        while (elapsedTime / duration < 1 /* 적이 없으면 */)
         {
-            elapsedTime +=  Time.deltaTime;
+            elapsedTime += Time.deltaTime;
             transform.position = Vector3.Lerp(currentPos, targetPosition, elapsedTime / duration);
-            //���߰�
+            //멈추고
             yield return null;// new WaitForSeconds(0.05f);
-            print("�����δ�");
+            //print("움직인다");
         }
-
         transform.position = targetPosition;
-
         yield return new WaitForSeconds(0.04f);
-        //anim.SetTrigger("Idle");
         anim.CrossFade("Idle", 0.5f, 0);
-
-        // ----------- �ϳѱ� ----------------
-        BoardManager.Instance.PieceIsMove = false;
-        // ----------- �ϳѱ� ----------------
-
-        //yield return new WaitForSeconds(1f);
-
+        //회전한만큼 회전시킨다.
         StartCoroutine(RotatePiece(-angle * nDir, (1.0f / 45) * angle, targetX, targetY, false));
 
 
     }
+    //회전 공식(완료)
     private IEnumerator RotatePiece(float targetAngle, float duration, int x, int y, bool moveFoward)
     {
-        if(duration > 0)
+        if (duration > 0)
         {
-
+            //자신의 각도
             float currentAngle = 0;// transform.eulerAngles.y;
-            float elapsedTime = 0f; 
+            float elapsedTime = 0f; // 시간
+                                    //시간이 흐르면
             while (elapsedTime < duration)
             {
+                //회전을 Lerp값으로 회전
                 float angle = Mathf.LerpAngle(currentAngle, targetAngle, elapsedTime / duration);
-                
+
                 transform.rotation = Quaternion.Euler(0, myAngle + angle, 0);
 
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
         }
-        
+
         myAngle = myAngle + targetAngle;
         transform.rotation = Quaternion.Euler(0f, myAngle, 0f);
         yield return new WaitForSeconds(1);
-        //StartCoroutine(StraightMove(0, 1));
 
-        if(moveFoward)
+        if (moveFoward)
         {
             StartCoroutine(StraightMove(x, y));
         }
-        //transform.rotation = Quaternion.Euler(0f, myAngle, 0f);
-
-        
     }
 }
